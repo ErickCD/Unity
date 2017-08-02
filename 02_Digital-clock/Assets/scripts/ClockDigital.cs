521cc2462de0b2383bb9f7bd1068d9532a83d4c0
Clair <acheron_1405@hotmail.com>
2017-08-02 13:33:33 -0500

    Cambios para ver el orden de los proyectos y terminado proyecto DisplayingAnImage

---
+++ 02_Digital-clock/Assets/scripts/ClockDigital.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using UnityEngine;
+
+using UnityEngine.UI;
+using System;
+
+public class ClockDigital : MonoBehaviour {
+
+    private Text textClock;
+
+	// Use this for initialization
+	void Start () {
+        textClock = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        DateTime time = DateTime.Now;
+        String hour = LeadingZero(time.Hour);
+        String minute = LeadingZero(time.Minute);
+        String second = LeadingZero(time.Second);
+
+        textClock.text = "La hora es: "+hour + ":" + minute + ":" + second;
+	}
+
+    String LeadingZero(int n){
+        return n.ToString().PadLeft(2, '0');
+    }
+}
