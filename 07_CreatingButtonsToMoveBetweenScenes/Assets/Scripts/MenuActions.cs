cf5c640a43441fbf35aa3ff0076d36e619b4215e
Clair <acheron_1405@hotmail.com>
2017-08-02 15:25:20 -0500

    Proyecto de movimiento de pantallas completo

---
+++ 07_CreatingButtonsToMoveBetweenScenes/Assets/Scripts/MenuActions.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class MenuActions : MonoBehaviour {
+
+    public void MENU_ACTION_GotoPage(string sceneName){
+        Application.LoadLevel(sceneName);
+        //SceneManager.LoadScene(sceneName);
+    }
+}
