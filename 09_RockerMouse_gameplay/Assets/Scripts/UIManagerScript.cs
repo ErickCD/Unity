1b82fe818f7628b9ed9707f3994be3ed53b18d7f
Clair <acheron_1405@hotmail.com>
2017-08-03 15:09:05 -0500

    Gameplay avanzado con action buttons, triggers and music

---
+++ 09_RockerMouse_gameplay/Assets/Scripts/UIManagerScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManagerScript : MonoBehaviour {
+
+    public Animator dialog;
+    public Animator startButton;
+    public Animator settingsButton;
+
+    public void StartGame() {
+        SceneManager.LoadScene("RocketMouse");
+    }
+
+    public void OpenSettings() {
+        startButton.SetBool("isHidden", true);
+        settingsButton.SetBool("isHidden", true);
+
+        //Is add sometime left.
+        dialog.SetBool("isHidden", false);
+    }
+
+    public void CloseSettings() {
+        startButton.SetBool("isHidden", false);
+        settingsButton.SetBool("isHidden", false);
+        dialog.SetBool("isHidden", true);
+    }
+}
