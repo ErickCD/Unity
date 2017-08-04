1cb679ee068da54765b613b0f9b2ba93aaa45416
Clair <acheron_1405@hotmail.com>
2017-08-04 11:51:50 -0500

    proyecto para la utilizar paneles y controlar el movimiento de los mismos, es un juego de cartas simple

---
+++ 10_OraganizingImagesinsidePanelsAndChangingPanelDepthsViaButtons/Assets/Scripts/arrangeActions.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class arrangeActions : MonoBehaviour {
+
+    private RectTransform panelRectTransform;
+
+	//Initializacion of a RectTransform
+	void Start () {
+        panelRectTransform = GetComponent<RectTransform>();
+	}
+
+    public void MoveDownOne() {
+        print("(Before change)" + gameObject.name + "sibiling index = " + panelRectTransform.GetSiblingIndex());
+
+        int concurrentSlibingIndex = panelRectTransform.GetSiblingIndex();
+        panelRectTransform.SetSiblingIndex(concurrentSlibingIndex - 1);
+
+        print("(after changes)" + gameObject.name + " Slibing index = " + panelRectTransform.GetSiblingIndex());
+    }
+
+    public void MoveUpOne() {
+        print("(Before change)" + gameObject.name + ", Slibin indix = " + panelRectTransform.GetSiblingIndex());
+
+        int concurrentSlibingindex = panelRectTransform.GetSiblingIndex();
+        panelRectTransform.SetSiblingIndex(concurrentSlibingindex + 1);
+
+        print("(After change)" + gameObject.name + ", Slibing index = "+ panelRectTransform.GetSiblingIndex());
+    }
+}
