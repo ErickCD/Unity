8495c67ec1414a22ca6cce49b069ca3356fa23a8
Clair <acheron_1405@hotmail.com>
2017-09-07 11:31:11 -0500

    Inicio de las pruebas y realizacion de los diseños y animacines hechos en Blender

---
+++ 15_PruebaCono/Assets/Scripts/ControlPanel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlPanel : MonoBehaviour {
+
+    public Animator ButtonPanel;
+
+    public void animacionPanel()
+    {
+        print("Este es el hidden: " + ButtonPanel.GetBool("hidden"));
+    }
+}
---
+++ 15_PruebaCono/Assets/Scripts/TransitionController.cs
@@ -0,0 +1,147 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TransitionController : MonoBehaviour {
+
+    public Animator ButtonPanel;
+    //Animator ConoExplodedView
+    public Animator ani;
+    //GameObject Slider
+    public Slider slider;
+    //varClas to control the speed 
+    private float speedSlider;
+
+    public void animacionPanel()
+    {
+        if (ButtonPanel.GetBool("isHidden"))
+        {
+            ButtonPanel.SetBool("isHidden", false);
+        }
+        else
+        {
+            ButtonPanel.SetBool("isHidden", true);
+        }
+        print("Este es el hidden: " + ButtonPanel.GetBool("isHidden"));
+    }
+
+    private void NecesarioParaCambio() {
+        //Para regresar a un estado quieto
+        ani.SetBool("Quieto", true);
+        //Viento normal
+        ani.SetBool("vientoB", false);
+        ani.SetBool("vientoM", false);
+        ani.SetBool("vientoA", false);
+        //Viento a la izquierda
+        ani.SetBool("vientoBL", false);
+        ani.SetBool("vientoML", false);
+        ani.SetBool("vientoAL", false);
+        //Viento a la derecha
+        ani.SetBool("vientoBR", false);
+        ani.SetBool("vientoMR", false);
+        ani.SetBool("vientoAR", false);
+
+        ani.SetBool("explosion", false);
+    }
+
+    private void cambioSlider() {
+        slider.value = 0;
+        //slider.minValue = -1;
+        //slider.maxValue = 1;
+        //ani.SetFloat("speed", 0);
+    }
+
+    public void ChangeTransition(string nameTrasition){
+        //Thread.Sleep(500);
+        switch (nameTrasition) {
+                //Para viento normal
+            case "vientoB":
+                NecesarioParaCambio();
+                cambioSlider();
+                ani.SetBool("vientoB", true);
+                ani.SetBool("Quieto", false);
+                break;
+            case "vientoM":
+                NecesarioParaCambio();
+                cambioSlider();
+                ani.SetBool("vientoM", true);
+                ani.SetBool("Quieto", false);
+                break;
+            case "vientoA":
+                NecesarioParaCambio();
+                cambioSlider();
+                ani.SetBool("vientoA", true);
+                ani.SetBool("Quieto", false);
+                break;
+            //Para viento a la derecha 
+            case "RvientoB":
+                NecesarioParaCambio();
+                cambioSlider();
+                ani.SetBool("vientoBR", true);
+                ani.SetBool("Quieto", false);
+                break;
+            case "RvientoM":
+                NecesarioParaCambio();
+                cambioSlider();
+                ani.SetBool("vientoMR", true);
+                ani.SetBool("Quieto", false);
+                break;
+            case "RvientoA":
+                NecesarioParaCambio();
+                cambioSlider();
+                ani.SetBool("vientoAR", true);
+                ani.SetBool("Quieto", false);
+                break;
+            //Para viento a la izquierda
+            case "LvientoB":
+                NecesarioParaCambio();
+                cambioSlider();
+                ani.SetBool("vientoBL", true);
+                ani.SetBool("Quieto", false);
+                break;
+            case "LvientoM":
+                NecesarioParaCambio();
+                cambioSlider();
+                ani.SetBool("Quieto", false);
+                ani.SetBool("vientoML", true);
+                break;
+            case "LvientoA":
+                NecesarioParaCambio();
+                cambioSlider();
+                ani.SetBool("Quieto", false);
+                ani.SetBool("vientoAL", true);
+                break;
+            case "exploded":
+                NecesarioParaCambio();
+                ani.SetBool("Quieto", false);
+                ani.SetBool("explosion", true);
+                //if (speedSlider > -0.1 || speedSlider < 0.1)
+                //{
+                //    speedSlider = 0.2f;
+                //}
+                break;
+            case "quieto":
+                NecesarioParaCambio();
+                break;
+        }
+    }
+
+    void Update() {
+        speedSlider = slider.value;
+        //Solo una impresión
+        if (speedSlider > 0.1 || speedSlider < -0.1)
+        {
+            print(speedSlider);
+            //ChangeTransition("quieto");
+            ChangeTransition("exploded");
+            ani.SetFloat("speed", speedSlider);
+        }
+        //if (speedSlider < 0.1 && speedSlider > -0.1)
+        //{
+        //    print(speedSlider);
+        //    //ChangeTransition("quieto");
+        //    ChangeTransition("exploded");
+        //    ani.SetFloat("speed", speedSlider);
+        //}
+    }
+}
